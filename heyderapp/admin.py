5fdad76c4cf8d422f31baf3505414686ac69e64b
unknown <nicat254memmedov@gmail.com>
2023-10-26 16:08:23 +0400

    22

--- heyderapp/admin.py
+++ heyderapp/admin.py
@@ -12,15 +12,18 @@ class MyTitleAdmin(admin.ModelAdmin):
     exclude = ('title','content')
 class MyNameAdmin(admin.ModelAdmin):
     exclude = ('name','content')
+    
+class MyNameAdminVideo(admin.ModelAdmin):
+    exclude = ('name','content','coverimage')
 class MyOnlyNameAdmin(admin.ModelAdmin):
-    exclude = ('name','content')
+    exclude = ('name','content','coverimage')
 class MyminiTitleAdmin(admin.ModelAdmin):
     exclude = ('minititle','title','content','content2','contentbig')
 admin.site.register(HomeHeader,MyTitleAdmin)
 admin.site.register(HomeHeaderVideo,MyOnlyNameAdmin)
 admin.site.register(Article,MyNameAdmin)
 admin.site.register(Blog,MyModelAdmin)
-admin.site.register(Video,MyNameAdmin)
+admin.site.register(Video,MyNameAdminVideo)
 admin.site.register(Photo,MyNameAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
