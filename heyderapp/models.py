d1eb93353355f2474351c00b5a834fd51d86c409
jamilajj <122780129+jamilajj@users.noreply.github.com>
2023-10-31 16:42:39 +0400

    22

--- heyderapp/models.py
+++ heyderapp/models.py
@@ -194,8 +194,8 @@ class Partners(BaseMixin):
     
 class Head(models.Model):
     title = models.CharField(verbose_name='head title',max_length=230)
-    image = models.ImageField(verbose_name='favicon')
-    logoheader = models.ImageField(null=True,blank=True,verbose_name='diger_butun_loqolar')
+    image = models.ImageField(verbose_name='favicon',null=True,blank=True)
+    logoheader = models.FileField(null=True,blank=True,verbose_name='diger_butun_loqolar')
     def __str__(self):
         return 'favicon'
     
