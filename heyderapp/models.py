3e5c323f67a4653acf246e1553421952caa9461a
unknown <nicat254memmedov@gmail.com>
2023-11-09 15:37:09 +0400

    22

--- heyderapp/models.py
+++ heyderapp/models.py
@@ -108,7 +108,7 @@ class Photo(BaseMixin):
     content = models.TextField()
     image = models.ImageField()
     ordering = models.IntegerField(null=True,blank=True)
-    inhome = models.BooleanField(defaul=False)
+    inhome = models.BooleanField(default=False)
     def __str__(self):
         return self.name + ' foto'
     
